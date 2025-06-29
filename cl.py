39494530566f7218dbf34db3a7fd6eda2a4d01b5
Arshiacomplus2 <arshianavkhsi@gmail.com>
2025-06-29 15:04:39 +0330

    Update cl.py

--- cl.py
+++ cl.py
@@ -30,8 +30,8 @@ TEXT_PATH="normal.txt"
 LINK_PATH=[] # [ "link1" , "link2" , ... ]
 FIN_PATH="final.txt"
 FIN_CONF=[]
-CHECK_LOC=True
-CHECK_IRAN=True
+CHECK_LOC=False
+CHECK_IRAN=False
 CHECK_HOST_IRANIAN_NODES = [
     "ir1.node.check-host.net",  # Tehran, AS44244 Mobile Communication Company of Iran (MCI)
     "ir2.node.check-host.net",  # Tehran, AS12880 Telecommunication Infrastructure Company (TIC زیرساخت)
